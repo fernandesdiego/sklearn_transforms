cfbf82a0b5ea6dc258629c298f19d30b9fdc48f8
Diego Fernandes <fernandes.diegod@gmail.com>
2020-08-18 16:56:29 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from imblearn.under_sampling import NearMiss
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -14,3 +14,43 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+#fill ingles
+def FillIngles(dataframe):
+    print("Preenchendo Ingles")
+    ingles_freq = dataframe['INGLES'].value_counts().index[0]
+    return dataframe.INGLES.fillna(ingles_freq)
+
+#remove quem não tem atividade nem hora de aula nem nota
+def RemoveDesistente(dataframe): 
+    print("Removendo desistente")
+    dataframe.drop(dataframe[(dataframe['NOTA_DE'] == 0) & (dataframe['NOTA_EM'] == 0) & (dataframe['NOTA_MF'] == 0) & (dataframe['NOTA_GO'] == 0) & (dataframe['H_AULA_PRES'] == 0) & (dataframe['TAREFAS_ONLINE'] == 0)].index, inplace=True)
+    return dataframe
+
+#fill nota go
+def FillGO(dataframe):
+    print("Preenchendo GO")
+    dataframe.NOTA_GO.fillna((dataframe['NOTA_DE'] + dataframe['NOTA_EM'] + dataframe['NOTA_MF'])/3)
+    return dataframe
+
+#add coluna media geral
+def AddMedia(dataframe):
+    print("Adicionando Media")
+    dataframe['MEDIA_GERAL'] = (dataframe['NOTA_DE'] + dataframe['NOTA_EM'] + dataframe['NOTA_MF'] + dataframe['NOTA_GO'])/4
+    return dataframe
+
+#add coluna media_humanas
+def AddMediaHumanas(dataframe):
+    print("Adicionando Media Humanas")
+    dataframe['MEDIA_HUMANAS'] = (dataframe['NOTA_DE'] + dataframe['NOTA_EM'])/2 
+
+#add coluna media_exatas
+def AddMediaExatas(dataframe):
+    print("Adicionando Media Exatas")
+    dataframe['MEDIA_EXATAS'] = (dataframe['NOTA_MF'] + dataframe['NOTA_GO'])/2
+    return dataframe
+
+def BalanceData(X,y):
+    print("Balanceando dados")
+    nr = NearMiss()
+    X, y = nr.fit_sample(X, y)    
